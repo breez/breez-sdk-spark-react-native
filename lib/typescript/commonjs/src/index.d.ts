21163d0a79867bfa86445c008340a4d752b57319
SDK release tagger <no-reply@breez.technology>
2025-11-12 15:15:45 +0000

    Update React Native bindings to version 0.4.0-rc2

--- lib/typescript/commonjs/src/index.d.ts
+++ lib/typescript/commonjs/src/index.d.ts
@@ -1,8 +1,11 @@
+export * from './generated/breez_sdk_common';
 export * from './generated/breez_sdk_spark';
 export * from './generated/breez_sdk_spark_bindings';
+import * as breez_sdk_common from './generated/breez_sdk_common';
 import * as breez_sdk_spark from './generated/breez_sdk_spark';
 import * as breez_sdk_spark_bindings from './generated/breez_sdk_spark_bindings';
 declare const _default: {
+    breez_sdk_common: typeof breez_sdk_common;
     breez_sdk_spark: typeof breez_sdk_spark;
     breez_sdk_spark_bindings: typeof breez_sdk_spark_bindings;
 };
